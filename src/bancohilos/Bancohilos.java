5a14eac5d0a24adeeaccd94d10309290b97ce0fb
alkeys <am18007@ues.edu.sv>
2021-03-23 12:46:12 -0600

    xd2

--- src/bancohilos/Bancohilos.java
+++ src/bancohilos/Bancohilos.java
@@ -1,12 +1,44 @@
 package bancohilos;
+
 public class Bancohilos {
 
     public static void main(String[] args) {
-  cuentas xD=new cuentas();
-        
-        
-        
-        
+        cuentas xD = new cuentas();
+        for (int i = 0; i < 100; i++) {
+            eje r = new eje(xD, i, 2000);
+            r.start();
+        }
+
     }
-    
+
+}
+
+class eje extends Thread {
+
+    public eje(cuentas c, int cuenta0, double max) {
+        banco = c;
+        cue0 = cuenta0;
+        cantida = max;
+    }
+
+    private cuentas banco;
+    private int cue0;
+    private int paracuenta;
+    private double cantida;
+
+    @Override
+    public void run() {
+
+        while (true) {
+            paracuenta = (int) (Math.random() * 100);
+            double cantida = this.cantida * Math.random();
+            banco.cuentasorigen(cue0, paracuenta, cantida);
+            try {
+                Thread.sleep((int) Math.random() * 10);
+            } catch (InterruptedException e) {
+            }
+        }
+
+    }
+
 }
--- src/bancohilos/cuentas.java
+++ src/bancohilos/cuentas.java
@@ -1,18 +1,46 @@
 package bancohilos;
+
 public class cuentas {
-private double  dinero;
-private double[] cuentas;
-    public cuentas(){
-    cuentas=new double[100];
-}
 
-public void inicio(){
-    
-    for (int i = 0; i < 100; i++) {
-        
+    private double dinero;
+    private double[] cuentas;
+
+    public cuentas() {
+        cuentas = new double[100];
     }
-}
 
+    public void inicio() {
 
+        for (int i = 0; i < 100; i++) {
+            cuentas[i] = 2000;
+        }
+    }
+
+    public void cuentasorigen(int cuenta0, int cuentadestino, double dinerotranferncia) {
+        if (cuentas[cuenta0] < dinerotranferncia) {
+            return;//no haga nada
+        }
+        double aux, aux2, aux3;
+        System.out.println(Thread.currentThread());//muestra el hilo de la tranferencia
+        aux = cuentas[cuenta0];
+        cuentas[cuenta0] -= dinerotranferncia;//quita el dinero de la cuenta oringen
+        aux2 = cuentas[cuenta0];
+        cuentas[cuentadestino] += dinerotranferncia;
+        aux3 = cuentas[cuentadestino];
+        System.out.println("dinero antes =" + aux + "   dinero de restante =" + aux2);
+        System.out.println("dinero de cuenta "+aux3);
+        System.out.println("el saldo total es"+mostrar());
+    }
+
+    
+    public double mostrar(){
+        double saldo=0;
+        for (double cuenta : cuentas) {
+            saldo+=cuenta;
+        }
+        
+        return saldo;
+    }
+    
     
 }
