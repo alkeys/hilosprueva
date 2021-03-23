6472b173c7dc65365de0b37c60f6eb7e88e2320a
alkeys <am18007@ues.edu.sv>
2021-03-23 12:10:59 -0600

    metodos

--- src/bancohilos/cuentas.java
+++ src/bancohilos/cuentas.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bancohilos;
-
-/**
- *
- * @author alexa
- */
 public class cuentas {
+private double  dinero;
+private double[] cuentas;
+    public cuentas(){
+    cuentas=new double[100];
+}
+
+public void inicio(){
+    
+    for (int i = 0; i < 100; i++) {
+        
+    }
+}
+
+
     
 }
